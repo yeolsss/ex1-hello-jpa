1fbfeb1471660b3ba4bfd58c62dfc5b4d896eaaf
yeol <yeol8810@gmail.com>
2023-01-04 11:26:54 +0900

    1차 캐시에 저장 후 1차 캐시에서 조회

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,15 +18,21 @@ public class JpaMain {
 
             //비영속 상태
             Member member = new Member();
-            member.setId(1L);
+            member.setId(101L);
             member.setName("helloJPA");
             //비영속 상태 끝
 
             //영속 상태
             // em.persist를 한다고 바로 db에 query를 날리지 않는다.
             System.out.println("=== BEFORE === ");
+            //1차 캐시에 저장
             em.persist(member);
             System.out.println("=== AFTER ===");
+
+            //1차 캐시에서 조회
+            Member findMember = em.find(Member.class, 101L);
+            System.out.println("findMember.getId() = " + findMember.getId());
+            System.out.println("findMember.getName() = " + findMember.getName());
 
             tx.commit();
         } catch (Exception e) {
