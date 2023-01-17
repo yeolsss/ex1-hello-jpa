bef4998762211f5819859df41734a7324b810f93
yeol <yeol8810@gmail.com>
2023-01-17 12:00:08 +0900

    객체 매핑, 필드와 컬럼 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,5 @@
 package hellojpa;
 
-import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -15,20 +14,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-
-            //영속 엔티티 조회
-            Member memberA = em.find(Member.class, 150L);
-            memberA.setName("AAA");
-
-            //준영속 엔티티 전환
-            //detach: 단일 객체만 준영속으로 전환
-            em.detach(memberA);
-            //EntityManager를 통째로 준영속으로 전환
-            //즉, 1차캐시를 다 지운다.
-            em.clear();
-            //영속성 컨텍스트를 닫는다.
-            em.close();
-
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,36 +1,42 @@
 package hellojpa;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
+//테이블명 매핑
+//@Table(name="MBR")
 public class Member {
 
     @Id
     private Long id;
-    private String name;
+    @Column(name = "name", updatable = false, nullable = false)
+    private String username;
+    @Column(precision = 19, scale = 2)
+    private Integer age;
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
 
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
+
+    @Lob
+    private String description;
     protected Member() {
     }
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
