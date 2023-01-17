75b431240ee5e3a23fecedbd7b2e489519e16122
yeol <yeol8810@gmail.com>
2023-01-17 13:46:38 +0900

    기본 키 매핑 - SEQUENCE 전략

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -12,7 +12,7 @@ import javax.persistence.Id;
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     @Column(name = "name")
     private String username;
